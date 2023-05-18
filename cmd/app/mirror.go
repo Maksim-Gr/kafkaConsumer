3cae55f3631248975f869599a16609d775ed7d1a
Maksim Grishchenko <maksim.g.grishchenko@gmail.com>
2023-05-18 23:05:23 +0300

    feat: implement consumerGroupSession methods

--- cmd/app/mirror.go
+++ cmd/app/mirror.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"github.com/Maksim-Gr/kafkaConsumer/pkg/util"
-	"gopkg.in/Shopify/sarama.v1"
+	"github.com/Shopify/sarama"
 	"log"
+	"strings"
+	"time"
 )
 
 var (
@@ -18,15 +21,39 @@ type Consumer struct {
 	ready chan bool
 }
 
+func (consumer *Consumer) Setup(sarama.ConsumerGroupSession) error {
+	close(consumer.ready)
+	return nil
+}
+
+func (consumer *Consumer) Cleanup(sarama.ConsumerGroupSession) error {
+	return nil
+}
+
+func (consumer *Consumer) ConsumeClaim(session sarama.ConsumerGroupSession, claim sarama.ConsumerGroupClaim) error {
+	for {
+		select {
+		case message := <-claim.Messages():
+			log.Printf("message value: %s", string(message.Value))
+			session.MarkMessage(message, "")
+
+		case <-session.Context().Done():
+			return nil
+		}
+	}
+}
+
 func main() {
 	keepRunning := true
 	log.Println("starting mirror consumer")
 
 	consumerConfig := sarama.NewConfig()
 	consumerConfig.Consumer.Offsets.Initial = sarama.OffsetNewest
-
-	for keepRunning {
-		select {}
+	consumerConfig.Consumer.MaxProcessingTime = 10 * time.Second
+	ctx, cancel := context.WithCancel(context.Background())
+	client, err := sarama.NewConsumerGroup(strings.Split(brokers, ","), group, consumerConfig)
+	if err != nil {
+		log.Panicf("error creating consumer group clinet: %v", err)
 	}
 
 	config, err := util.LoadConfig(".")
